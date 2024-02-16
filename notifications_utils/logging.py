467d4a66d83f8be549eecea5a7201676ecd9b62f
Pier-Angelo Gaetani <pgaetani@worth.systems>
2024-02-16 16:18:13 +0100

    Application insights revert

--- notifications_utils/logging.py
+++ notifications_utils/logging.py
@@ -9,7 +9,6 @@ from typing import Sequence
 
 from flask import current_app, g, request
 from flask.ctx import has_app_context, has_request_context
-from opencensus.ext.azure.log_exporter import AzureLogHandler
 from pythonjsonlogger.jsonlogger import JsonFormatter as BaseJSONFormatter
 
 LOG_FORMAT = (
@@ -96,9 +95,9 @@ def init_app(app, statsd_client=None, extra_filters: Sequence[logging.Filter] = 
 
         return response
 
-    del app.logger.handlers[:]
+    logging.getLogger().addHandler(logging.NullHandler())
 
-    logging.getLogger().addHandler(AzureLogHandler())
+    del app.logger.handlers[:]
 
     if app.config["NOTIFY_RUNTIME_PLATFORM"] != "ecs":
         # TODO: ecs-migration: check if we still need this function after we migrate to ecs
